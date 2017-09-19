0029f6690e908b213a96097d32a0a2e65ac95a67
Prathima Bandla <prathima.bandla@openx.com>
2017-09-18 17:33:24 -0700

    Test project for circle ci

---
+++ src/main/java/org/circle/test/circle_ci_test/App.java
@@ -0,0 +1,13 @@
+package org.circle.test.circle_ci_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
