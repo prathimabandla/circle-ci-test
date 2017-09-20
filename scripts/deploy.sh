037f623a68159473c2b651fcd34be01da4ae6e5a
Prathima Bandla <prathima.bandla@openx.com>
2017-09-20 13:53:49 -0700

    Testing deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ echo "GCLOUD_PROJECT : $GCLOUD_PROJECT"
 
 curl -o $HOME/google-cloud-sdk_172.0.0.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-172.0.0-linux-x86_64.tar.gz
 tar -xvzf $HOME/google-cloud-sdk_172.0.0.tar.gz -C $HOME/
-sh $HOME/google-cloud-sdk/install.sh --disable-prompts
+sh $HOME/google-cloud-sdk/install.sh --usage-reporting false
