7b7049b0f68444804e70681b68f2e9911ec87c56
Prathima Bandla <prathima.bandla@openx.com>
2017-09-20 18:13:29 -0700

    Testing out a different deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,3 @@
 
 echo "ENV : $ENV"
 echo "GCLOUD_PROJECT : $GCLOUD_PROJECT"
-
-curl -o $HOME/google-cloud-sdk_172.0.0.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-172.0.0-linux-x86_64.tar.gz
-tar -xvzf $HOME/google-cloud-sdk_172.0.0.tar.gz -C $HOME/
-sh $HOME/google-cloud-sdk/install.sh --usage-reporting false
